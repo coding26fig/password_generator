f15590f7c8da4089c88f4d3b16ca237606a3f94b
Charnay Birton <coding26fig@gmail.com>
2022-03-19 19:52:58 -0400

    commenting to understand

--- script.js
+++ script.js
@@ -5,30 +5,41 @@ var upperCase =["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,U,R,S,T,U,W,X,Y,Z"]
 var lowerCase =["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
 var characters =["!,@,#,$,%,^,&,*,(,),_,+,<,>,?,{,}"]
 var length=("8")
-var passwordValue =("")
+var passwordValue =[""]
 var generateBtn = document.querySelector("#generate")
 
 
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  }
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-function generatePassword(){
-
-}
 
 function getPrompt(){
-  length = prompt("How many characters would you like your password to be? (8 - 128"))
+  length = parseInt(prompt("How many characters would you like your password to be? (8 - 128"))
+}
+if (confirm("Would you like lowercase letters in your password")){
+  passwordValue = passwordValue.concat(lowerCase)
+}  
+if (confirm("Would you like uppercase letters in your password")){
+  passwordValue = passwordValue.concat(upperCase)
+}  
+
+if (confirm("Would you like numbers  in your password")){
+  passwordValue = passwordValue.concat(numbers)
+}  
+if (confirm("Would you like special characters in your password")){
+  passwordValue = passwordValue.concat(characters)
+}  
 
 
- }
+
