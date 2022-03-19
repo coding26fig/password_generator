f98e4d8b340edd5bd68291bf0af4e98950f17d67
Charnay Birton <coding26fig@gmail.com>
2022-03-19 15:04:58 -0400

    added var for password

---
+++ script.js
@@ -0,0 +1,23 @@
+// Assignment Code
+
+var numbers = ["0,1,2,3,4,5,6,7,8,9"]
+var upperCase =["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,U,R,S,T,U,W,X,Y,Z"]
+var lowerCase =["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
+var characters =["!,@,#,$,%,^,&,*,(,),_,+,"]
+var lenght=("")
+var passwordValue =("")
+var generateBtn = document.querySelector("#generate")
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
