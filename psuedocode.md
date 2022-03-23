b0eb1e0bbd6725b7f2636856df7001867d0caa8b
Charnay Birton <coding26fig@gmail.com>
2022-03-23 19:33:13 -0400

    stating again

---
+++ psuedocode.md
@@ -0,0 +1,31 @@
+Prompts 
+prompt  1  how long would you like your password to be 
+    *conditions if the length of the password is less that 8 than give alert 
+    *condition if the lenght of the password is more than 128 than give an alert
+    * create a variable 
+    * save the result of the prompt to that variable
+
+confirm 2 Do you want to include numbers in your password?
+    *If user answers yes save it to a variable to hold this info
+
+var num
+var char
+var upper
+var lower
+****look into objects to store info with the variables
+
+
+
+Generate Password 
+
+if () variable from line 11 are true then  pick random element from array. { which is where you may use the math.floor or random generator to choose a variable from the array}
+
+Repeat line steps from line 21 for var 11 - 14. 
+
+Find a method that adds elements to a blank array (which is the passwordValue array we created)
+
+create a for loop to go thru the (passwordValue array) and it will only go thru the length of the variable created in line 5. 
+
+create another variable for the final result. This variable will 
+
+
--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-var length=8;
+var length = 8;
 var passwordValue =[]
-
+//Array of option for the computer to pick from. 
 var numbers = ['0','1','2','3','4','5','6','7','8','9']
 var upperCase =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','U','R','S','T','U','W','X','Y','Z']
 var lowerCase =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var specialCharacters =['!','@','#','$','%','^','&','*','(','_','+','<','>','?','{','}'] 
 
-var generateBtn = document.querySelector("#generate")
+var generateBtn = document.querySelector("#generate") // assigning element and assigning it into a variable. 
+//creating generatePassword function as it is called below.
+function generatePassword (){
 
-
+  return passwordValue
+}
 
 // Write password to the #password input
 function writePassword() {
-  getPrompts();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword();            //This function creates the password and is being called but never defined. 
+  var passwordText = document.querySelector("#password"); // element selected from html assigning to variable passwordText
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // assinging event listener to line 9 variable. this is saying
+//listen for the event "click" ,(the execute the function "write password")
 
 
-//Creating popup boxes 
+//Creating prompt 
 function getPrompts(){
   passwordValue = [];
   length = (prompt("How many characters would you like your password to be?"))
-  if(length > 8 || length < 128)
-  alert("Please try again")
-  return false;
+  if(length < 8 || length > 128)
+  alert("Please try again. Password must be between 8 -128 Characters")
+  ;
   
 }
-if(confirm("Would you like numbers in your password?"))
+if(confirm("Would you like numbers in your password?")) //ask the user if they would like numbers
 passwordValue = passwordValue.concat(numbers)
-if(confirm("Would you like uppercase letters in your password?"))
+
+if(confirm("Would you like uppercase letters in your password?")) // ask the user if they would like uppercase letters
 passwordValue = passwordValue.concat(upperCase)
-if(confirm("Would you like lowercase letters in your password?"))
+
+if(confirm("Would you like lowercase letters in your password?")) // ask the user if they would like lowercase letters
 passwordValue = passwordValue.concat(lowerCase)
+
 if (confirm("Would you like special characters in your password?"))
 passwordValue = passwordValue.concat(specialCharacters)
+
